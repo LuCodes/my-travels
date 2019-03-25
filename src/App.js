1f51bf5342b4c6243efd303c76e9cc28c0f8ba32
Lucas Blamont <lblamont@mac.com>
2019-03-25 11:06:02 +0100

    MAJ

--- src/App.js
+++ src/App.js
@@ -9,16 +9,24 @@ class App extends Component {
     return (
       <div className="App">
       <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src="https://blogvoyages.fr/wp-content/uploads/2014/05/voyage.jpg" alt="image3" />
       <h1 className="App-title">Travel</h1>
       </header>
       <Travel
-          distance="465,6 km via A6"
-          destination="France"
-          ville="Paris"
+      distance="465,6 km via A6"
+      destination="France"
+      ville="Paris"
 
-          image="http://s1.lprs1.fr/images/2016/07/20/5980563_1-0-439018451_1000x625.jpg"
-        />
+      image="http://s1.lprs1.fr/images/2016/07/20/5980563_1-0-439018451_1000x625.jpg"
+      />
+
+      <Travel
+      distance2="195,3 km via A6"
+      destination2="France"
+      ville2="Dijon"
+
+      image2="https://diplomeo-static.com/news-files/14000/14400/14457/Dijon%2C_France_%2826304675813%29.jpg"
+      />
       </div>
       );
     }
