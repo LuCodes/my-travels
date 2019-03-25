08dbde079154d2e1002c5dc0f46516435fb2ebc7
Lucas Blamont <lblamont@mac.com>
2019-03-25 10:46:37 +0100

    MAJ

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Travel from "./Travel";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+      <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Travel</h1>
+      </header>
+      <Travel
+          distance="465,6 km via A6"
+          destination="France"
+          ville="Paris"
 
-export default App;
+          image="http://s1.lprs1.fr/images/2016/07/20/5980563_1-0-439018451_1000x625.jpg"
+        />
+      </div>
+      );
+    }
+  }
+
+  export default App;
---
+++ src/Travel.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Travel = ({ destination, ville, distance,  image }) => (
+  <figure>
+    <img src={image} alt={destination} />
+    <figcaption>
+      <p>{ville}</p>
+      <p>{destination}</p>
+      <p>{distance}</p>
+    </figcaption>
+  </figure>
+);
+
+export default Travel;
